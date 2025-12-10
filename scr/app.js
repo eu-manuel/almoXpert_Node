c1b811f0b18de68223075566dd343b2e9af134eb
eu-manuel <emanueljunqueira120@gmail.com>
2025-12-10 09:41:44 -0300

    feat: Refactor app, separando a funcionalidade de inicar o servidor para o index.js

--- scr/app.js
+++ scr/app.js
@@ -12,17 +12,15 @@ const itemRoutes = require("./routes/itemRoutes");
 const authRoutes = require("./routes/authRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
 const supplierRoutes = require("./routes/supplierRoutes");
-const WarehouseRoutes = require("./routes/warehouseRoutes");
+const warehouseRoutes = require("./routes/warehouseRoutes");
 const movementRoutes = require("./routes/movementRoutes");
 const permissionsRoutes = require("./routes/permissionRoutes");
 
-
-//tabelas de relacionamento
+// tabelas de relacionamento
 const itemCategoryRoutes = require("./routes/itemCategoryRoutes");
 const itemSupplierRoutes = require("./routes/itemSupplierRoutes");
 const itemWarehouseRoutes = require("./routes/itemWarehouseRoutes");
 const userPermissionRoutes = require("./routes/userPermissionRoutes");
-
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
@@ -30,17 +28,13 @@ app.use("/api/items", itemRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/supplier", supplierRoutes);
 app.use("/api/moviment", movementRoutes);
-app.use("/api/warehouse", WarehouseRoutes);
+app.use("/api/warehouse", warehouseRoutes);
 app.use("/api/permissions", permissionsRoutes);
 
-//tabelas de relacionamento
+// tabelas de relacionamento
 app.use("/api/item-categories", itemCategoryRoutes);
 app.use("/api/item-suppliers", itemSupplierRoutes);
 app.use("/api/item-warehouses", itemWarehouseRoutes);
 app.use("/api/user-permissions", userPermissionRoutes);
 
-
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Servidor rodando na porta ${process.env.PORT || 3000}`);
-});
+module.exports = app;
