d6e4e5e05204bab256695a38e1b67c495bad51df
eu-manuel <emanueljunqueira120@gmail.com>
2026-01-05 09:33:44 -0300

    refactor: Remoção dos campos de timeStamps dos seeders

--- seeders/20250930001705-demo-item.js
+++ seeders/20250930001705-demo-item.js
@@ -4,9 +4,9 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Items', [
-      { nome: 'Parafuso', descricao: 'Parafuso 5mm', codigo_interno: 'PFS-001', unidade_medida: 'un', estoque_minimo: 100, estoque_maximo: 1000, status: 'ativo', data_cadastro: new Date(), createdAt: new Date(), updatedAt: new Date() },
-      { nome: 'Porca', descricao: 'Porca 5mm', codigo_interno: 'PNC-001', unidade_medida: 'un', estoque_minimo: 100, estoque_maximo: 800, status: 'ativo', data_cadastro: new Date(), createdAt: new Date(), updatedAt: new Date() },
-      { nome: 'Cabo de Rede', descricao: 'Cabo Cat6 1m', codigo_interno: 'CNR-001', unidade_medida: 'un', estoque_minimo: 50, estoque_maximo: 300, status: 'ativo', data_cadastro: new Date(), createdAt: new Date(), updatedAt: new Date() },
+      { nome: 'Parafuso', descricao: 'Parafuso 5mm', codigo_interno: 'PFS-001', unidade_medida: 'un', estoque_minimo: 100, estoque_maximo: 1000, status: 'ativo' },
+      { nome: 'Porca', descricao: 'Porca 5mm', codigo_interno: 'PNC-001', unidade_medida: 'un', estoque_minimo: 100, estoque_maximo: 800, status: 'ativo' },
+      { nome: 'Cabo de Rede', descricao: 'Cabo Cat6 1m', codigo_interno: 'CNR-001', unidade_medida: 'un', estoque_minimo: 50, estoque_maximo: 300, status: 'ativo' },
     ], {});
   },
 
